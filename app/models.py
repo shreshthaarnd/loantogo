d85ead231962a67d131108eac4a2fae0fdbd1fc4
unknown <shreshtharnd20@gmail.com>
2020-07-24 12:46:53 +0530

    aryan commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+	Name=models.CharField(max_length=100)
+	Address=models.CharField(max_length=200)
+	Phone=models.CharField(max_length=15)
+	Identity=models.CharField(max_length=50)
+	Reason=models.CharField(max_length=100)
+	class Meta:
+		db_table="Data"
--- app/views.py
+++ app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import *
+from app.models import *
+from django.http import *
 # Create your views here.
 def index(request):
 	return render(request,'index.html',{})
+@csrf_exempt
+def savedata(request):
+	if request.method=='POST':
+		name=request.POST.get('name')
+		address=request.POST.get('address')
+		phone=request.POST.get('phone')
+		identity=request.POST.get('identity')
+		reason=request.POST.get('reason')
+		obj=Data(
+			Name=name,
+			Address=address,
+			Phone=phone,
+			Identity=identity,
+			Reason=reason
+			)
+		obj.save()
+		return HttpResponse("<script>alert('Data Saved!'); window.location.replace('/index/')</script>")
+	else:
+		return Http404
